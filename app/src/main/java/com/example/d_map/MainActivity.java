43ffb64fafa7ee56fe68b3e464bd1faeb13ce680
Alfaza Ganz <alfazaranggana.n@gmail.com>
2021-12-08 22:10:23 +0700

    [nambah list dan search bar]

--- app/src/main/java/com/example/d_map/MainActivity.java
+++ app/src/main/java/com/example/d_map/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.d_map;
 
 import android.app.Activity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
@@ -13,20 +24,51 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapquest.mapping.MapQuest;
 import com.mapquest.mapping.maps.MapView;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends Activity {
+    private static final String TAG = "";
     private final LatLng MAPQUEST_HEADQUARTERS_LOCATION = new LatLng(
             -6.8749534061560915, 107.60492399711352);
     //latitude dan longitude diatur mengikuti lokasi UNPAR.
     private MapView mMapView;
     private MapboxMap mMapboxMap;
+    private EditText et_search;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MapQuest.start(getApplicationContext());
         setContentView(R.layout.activity_main);
-
+        et_search = (EditText) findViewById(R.id.et_search);
         mMapView = (MapView) findViewById(R.id.mapquestMapView);
+
+
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autocomplete_search);
+        String[] countries = getResources().getStringArray(R.array.locations_array);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
+        textView.setAdapter(adapter);
+
+        et_search.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                et_search.setHint("");
+                return false;
+            }
+        });
+
+        et_search.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    et_search.setHint("");
+                }
+            }
+        });
 
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(new OnMapReadyCallback() {
@@ -36,8 +78,50 @@ public class MainActivity extends Activity {
                 mMapView.setStreetMode();
                 mMapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(MAPQUEST_HEADQUARTERS_LOCATION, 11));
                 addMarker(mapboxMap);
+
+                init();
             }
         });
+
+    }
+
+    private void init() {
+        Log.d(TAG, "init : initializing");
+
+        et_search.setOnEditorActionListener((new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int action_id, KeyEvent keyEvent) {
+                if(action_id == EditorInfo.IME_ACTION_SEARCH || action_id == EditorInfo.IME_ACTION_DONE
+                        || keyEvent.getAction() == KeyEvent.ACTION_DOWN || keyEvent.getAction() == KeyEvent.KEYCODE_ENTER) {
+
+                    //Nanti cari di mapquest buat function search locationnya ditaro disini
+
+                    locate();
+                }
+                return false;
+            }
+        }));
+    }
+
+    private void locate() {
+        Log.d(TAG, "locating");
+
+        String searchString = et_search.getText().toString();
+
+        Geocoder geocoder = new Geocoder(MainActivity.this);
+        List<Address> list = new ArrayList<>();
+
+        try {
+            list = geocoder.getFromLocationName(searchString, 1);
+        } catch (IOException e) {
+            Log.e(TAG, "locate: IOException: " + e.getMessage());
+        }
+
+        if(list.size() > 0) {
+            Address address = list.get(0);
+
+            Log.d(TAG, "found location : " + address.toString());
+        }
     }
 
     CameraPosition position = new CameraPosition.Builder()
